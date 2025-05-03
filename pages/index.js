439ceddfe8586ac376a54c1db838b2eb31a31beb
Andrey Garkusha <105675656+tarkusha@users.noreply.github.com>
2025-05-03 18:04:41 +0000

    Initial commit with Snake Game placeholder

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "50px" }}>
+        <h1>🐍 Snake Game (Coming Soon)</h1>
+        <img
+          src="/snake-placeholder.png"
+          alt="Snake Game Placeholder"
+          style={{ maxWidth: "300px", marginTop: "20px" }}
+        />
+      </div>
+    );
+  }
+  
